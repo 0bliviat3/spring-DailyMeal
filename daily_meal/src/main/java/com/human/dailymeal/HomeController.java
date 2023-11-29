4e21bc8c353cf0cbb4f2ef76aeac79699c636ac5
Lee GeonHa <idkr3067@naver.com>
2023-11-29 14:34:58 +0900

    asd

--- daily_meal/src/main/java/com/human/dailymeal/HomeController.java
+++ daily_meal/src/main/java/com/human/dailymeal/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		//hiqweqweqwe123
+		//hiqweqweqwe123asd
 		return "home";
 	}
 	
