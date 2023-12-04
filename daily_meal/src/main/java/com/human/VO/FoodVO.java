15e514e787cd111805e67ff903a92311ab435f4c
song-jae-wan <sjo9810@gmail.com>
2023-12-04 15:52:57 +0900

    feat: VO 생성
    - foodVO, foodDetailVO, menuVO, menuDetailVO

---
+++ daily_meal/src/main/java/com/human/VO/FoodVO.java
@@ -0,0 +1,50 @@
+package com.human.VO;
+
+public class FoodVO {
+	
+	private int food_no;
+	private String food_name;
+	private int food_date;
+	private int food_allergy;
+	private int food_cost;
+	
+	public int getFood_no() {
+		return food_no;
+	}
+	
+	public void setFood_no(int food_no) {
+		this.food_no = food_no;
+	}
+	
+	public String getFood_name() {
+		return food_name;
+	}
+	
+	public void setFood_name(String food_name) {
+		this.food_name = food_name;
+	}
+	
+	public int getFood_date() {
+		return food_date;
+	}
+	
+	public void setFood_date(int food_date) {
+		this.food_date = food_date;
+	}
+	
+	public int getFood_allergy() {
+		return food_allergy;
+	}
+	
+	public void setFood_allergy(int food_allergy) {
+		this.food_allergy = food_allergy;
+	}
+	
+	public int getFood_cost() {
+		return food_cost;
+	}
+	
+	public void setFood_cost(int food_cost) {
+		this.food_cost = food_cost;
+	}
+}
