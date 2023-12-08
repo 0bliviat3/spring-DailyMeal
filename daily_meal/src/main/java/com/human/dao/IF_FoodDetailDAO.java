36a6f301e6cc34b24c2e0fc3f1a6afb7a7f89fc8
song-jae-wan <sjo9810@gmail.com>
2023-12-08 14:49:17 +0900

    fix: dao 접근제어자 수정

--- daily_meal/src/main/java/com/human/dao/IF_FoodDetailDAO.java
+++ daily_meal/src/main/java/com/human/dao/IF_FoodDetailDAO.java
@@ -7,9 +7,9 @@ import org.springframework.dao.DataAccessException;
 import com.human.VO.FoodDetailVO;
 
 public interface IF_FoodDetailDAO {
-	void insertFoodDetail(FoodDetailVO foodDetailVO) throws DataAccessException, Exception;
-	List<FoodDetailVO> selectFood(int food_no) throws DataAccessException, Exception;
-	List<FoodDetailVO> selectAll() throws DataAccessException, Exception;
-	void updateFoodCnt(FoodDetailVO foodDetailVO) throws DataAccessException, Exception;
-	void deleteFoodDetail(FoodDetailVO foodDetailVO) throws DataAccessException, Exception;
+	public void insertFoodDetail(FoodDetailVO foodDetailVO) throws DataAccessException, Exception;
+	public List<FoodDetailVO> selectFood(int food_no) throws DataAccessException, Exception;
+	public List<FoodDetailVO> selectAll() throws DataAccessException, Exception;
+	public void updateFoodCnt(FoodDetailVO foodDetailVO) throws DataAccessException, Exception;
+	public void deleteFoodDetail(FoodDetailVO foodDetailVO) throws DataAccessException, Exception;
 }
