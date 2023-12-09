34b84bdc5572c89b9eb91053ea18432fb711a945
song-jae-wan <sjo9810@gmail.com>
2023-12-09 12:48:34 +0900

    feat: 서비스 객체주입 확인작업 구현
    - 컨테이너가 정상작동하는지 확인

--- daily_meal/src/main/java/com/human/dailymeal/HomeController.java
+++ daily_meal/src/main/java/com/human/dailymeal/HomeController.java
@@ -4,27 +4,26 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.human.VO.UserVO;
+import com.human.service.IF_DailyMealService;
+
+
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Inject
+	private IF_DailyMealService dailyMealService;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -34,6 +33,14 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 
 		return "home"; 
+	}
+	
+	@RequestMapping(value = "/input", method = RequestMethod.POST)
+	public String BMRDetails(Locale locale, Model model,
+			@ModelAttribute("") UserVO userVO) {
+		// TODO: 객체 주입 확인용 메소드... 수정 필요
+		System.out.println(dailyMealService.calculateBMR(userVO));
+		return null;
 	}
 	
 }
