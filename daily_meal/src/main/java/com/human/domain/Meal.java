4937c681a4474d8a83b898902d4b2fe3a8cadd9f
song-jae-wan <sjo9810@gmail.com>
2023-12-11 12:56:48 +0900

    feat: 식단 일급 컬렉션 구현

---
+++ daily_meal/src/main/java/com/human/domain/Meal.java
@@ -0,0 +1,16 @@
+package com.human.domain;
+
+import java.util.Map;
+
+import com.human.VO.MenuVO;
+import com.human.domain.constant.MealConstant;
+
+public class Meal {
+	
+	private final Map<MealConstant, MenuVO> meal;
+	
+	public Meal(final Map<MealConstant, MenuVO> meal) {
+		this.meal = meal;
+	}
+
+}
