a205d47ec5b61029c638ef156f9a42c5130cf8e7
song-jae-wan <sjo9810@gmail.com>
2023-12-12 00:11:56 +0900

    feat: meal 객체 메소드 선언
    - 가격
    - 칼로리
    - json 객체 반환

--- daily_meal/src/main/java/com/human/domain/Meal.java
+++ daily_meal/src/main/java/com/human/domain/Meal.java
@@ -2,6 +2,8 @@ package com.human.domain;
 
 import java.util.Map;
 
+import org.json.simple.JSONArray;
+
 import com.human.VO.MenuVO;
 import com.human.domain.constant.MealConstant;
 
@@ -13,7 +15,18 @@ public class Meal {
 		this.meal = meal;
 	}
 	
-	public MenuVO getName() {
-		return meal.get(MealConstant.RICE);
+	public int getCost() {
+		// TODO: 식단의 총 금액 리턴
+		return 0;
+	}
+	
+	public int getKcal() {
+		// TODO: 식단의 총 칼로리 리턴
+		return 0;
+	}
+	
+	public JSONArray getMealJson() {
+		// TODO: json 형식으로 데이터 폼 만들어 리턴하기
+		return null;
 	}
 }
