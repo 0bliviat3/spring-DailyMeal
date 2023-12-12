ac7cfbe7ca81d3e8b517a15e38ebc45c0ab14197
song-jae-wan <sjo9810@gmail.com>
2023-12-12 23:00:42 +0900

    fix: java 컨벤션에 맞게 수정

--- daily_meal/src/main/java/com/human/domain/Meal.java
+++ daily_meal/src/main/java/com/human/domain/Meal.java
@@ -16,37 +16,37 @@ public class Meal {
 	private static final String TYPE = "menu_type";
 	private static final String INFO = "menu_info";
 	private final Map<MealConstant, MenuVO> meal;
-	
+
 	public Meal(final Map<MealConstant, MenuVO> meal) {
 		this.meal = meal;
 	}
-	
+
 	public int getCost() {
 		int sum = 0;
-		for(MenuVO mvo : meal.values()) {
+		for (MenuVO mvo : meal.values()) {
 			sum += mvo.getMenu_cost();
 		}
 		return sum;
 	}
-	
+
 	public int getKcal() {
 		int sum = 0;
-		for(MenuVO mvo : meal.values()) {
+		for (MenuVO mvo : meal.values()) {
 			sum += mvo.getMenu_kcal();
 		}
 		return sum;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public JSONArray getMealJson() throws ParseException {
 		JSONArray mealJson = new JSONArray();
 		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 		JSONParser parser = new JSONParser();
-		for(MealConstant mealconstant : meal.keySet()) {
+		for (MealConstant mealconstant : meal.keySet()) {
 			JSONObject menuJson = new JSONObject();
 			menuJson.put(TYPE, mealconstant.getMenu());
 			String jsonStr = gson.toJson(meal.get(mealconstant));
-			JSONObject jsonObj = (JSONObject)parser.parse(jsonStr);
+			JSONObject jsonObj = (JSONObject) parser.parse(jsonStr);
 			menuJson.put(INFO, jsonObj);
 			mealJson.add(menuJson);
 		}
