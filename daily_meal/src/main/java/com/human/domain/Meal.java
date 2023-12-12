de877d041ed4f3487e13b0852fb05d0a73bc26fb
song-jae-wan <sjo9810@gmail.com>
2023-12-12 17:49:51 +0900

    feat: meal 객체의 기능 구현
    - 가격총합 계산
    - 칼로리 총합 계산
    - json객체로 변환

--- daily_meal/src/main/java/com/human/domain/Meal.java
+++ daily_meal/src/main/java/com/human/domain/Meal.java
@@ -3,12 +3,18 @@ package com.human.domain;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.human.VO.MenuVO;
 import com.human.domain.constant.MealConstant;
 
 public class Meal {
-	
+	private static final String TYPE = "menu_type";
+	private static final String INFO = "menu_info";
 	private final Map<MealConstant, MenuVO> meal;
 	
 	public Meal(final Map<MealConstant, MenuVO> meal) {
@@ -16,17 +22,34 @@ public class Meal {
 	}
 	
 	public int getCost() {
-		// TODO: 식단의 총 금액 리턴
-		return 0;
+		int sum = 0;
+		for(MenuVO mvo : meal.values()) {
+			sum += mvo.getMenu_cost();
+		}
+		return sum;
 	}
 	
 	public int getKcal() {
-		// TODO: 식단의 총 칼로리 리턴
-		return 0;
+		int sum = 0;
+		for(MenuVO mvo : meal.values()) {
+			sum += mvo.getMenu_kcal();
+		}
+		return sum;
 	}
 	
-	public JSONArray getMealJson() {
-		// TODO: json 형식으로 데이터 폼 만들어 리턴하기
-		return null;
+	@SuppressWarnings("unchecked")
+	public JSONArray getMealJson() throws ParseException {
+		JSONArray mealJson = new JSONArray();
+		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+		JSONParser parser = new JSONParser();
+		for(MealConstant mealconstant : meal.keySet()) {
+			JSONObject menuJson = new JSONObject();
+			menuJson.put(TYPE, mealconstant.getMenu());
+			String jsonStr = gson.toJson(meal.get(mealconstant));
+			JSONObject jsonObj = (JSONObject)parser.parse(jsonStr);
+			menuJson.put(INFO, jsonObj);
+			mealJson.add(menuJson);
+		}
+		return mealJson;
 	}
 }
